f3ffe17d60d2a814ba7fac3595b2f6b10f7a4587
santiago01est <santiago01est@gmail.com>
2023-10-12 10:53:24 -0500

    anexo de estructura de carpetas

--- lib/pages/screen_welcome/welcome.dart
+++ lib/pages/screen_welcome/welcome.dart
@@ -6,12 +6,31 @@ class welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        child: Image.asset(
-          'assets/Inicio.png',
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/Inicio.png',
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 20,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: IconButton(
+                onPressed: () {
+                  print('Boton presionado');
+                },
+                iconSize: 100,
+                icon: Image.asset(
+                  'assets/Bttn.png',
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
