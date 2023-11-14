3e0569e4117e59d388c1b91d79a2c5571f58be89
santiago01est <santiago01est@gmail.com>
2023-11-14 12:00:03 -0500

    firebase delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:dev_tesis/config/config.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'firebase_options.dart';
 import 'constants/styles.dart';
 
 void main() {
@@ -9,7 +10,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
