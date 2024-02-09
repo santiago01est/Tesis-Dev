c16041de9d7f295e443d9cf7303ea6f5893db859
santiago01est <santiago01est@gmail.com>
2024-02-09 12:41:36 -0500

    anexo diseño de actividades de laberinto v1 para escritorio

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Color sixtyColor = const Color(0xFFF7F5F5);
 Color thirtyColor = Color.fromARGB(255, 221, 232, 240);
-Color blueColor = const Color(0xFFACD8ED);
+Color blueColor = Color.fromARGB(255, 164, 212, 234);
 Color blueDarkColor = Color.fromARGB(255, 2, 76, 111);
 Color orangeColor = const Color(0xFFF5C3A8);
 Color greenColor = Color.fromARGB(255, 152, 246, 191);
--- lib/ui/widgets/response_game_flame.dart
+++ lib/ui/widgets/response_game_flame.dart
@@ -9,19 +9,20 @@ class RespuestaLaberinto extends StatelessWidget {
   Widget build(BuildContext context) {
     final movementInstructionsCubit = context.watch<InstruccionesCubit>();
     return Container(
-      margin: const EdgeInsets.all(30),
+      margin: const EdgeInsets.all(10),
       width: double.infinity,
-      height: 300,
+      height: 200,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Colors.white,
         border: Border.all(color: Colors.black, width: 2),
       ),
       child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 6, // Cambia esto según la cantidad deseada de columnas
-          mainAxisSpacing: 8.0
-        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            // si el ancho de la pantalla es mayor a 700 es un dispositivo escritorio y las columnas son 6
+            crossAxisCount: MediaQuery.of(context).size.width > 900 ? 6 : 3,
+            // Cambia esto según la cantidad deseada de columnas
+            mainAxisSpacing: 2.0),
         itemCount: movementInstructionsCubit.state.length,
         itemBuilder: (context, index) {
           final clave = movementInstructionsCubit.state[index].key;
@@ -31,12 +32,13 @@ class RespuestaLaberinto extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                Text(clave),
+                //Text(clave),
                 Image.asset(
-                    valor,
-                    width: 60, // Ajusta el tamaño de la imagen según sea necesario
-                    height: 60,
-                  ),
+                  valor,
+                  width:
+                      50, // Ajusta el tamaño de la imagen según sea necesario
+                  height: 50,
+                ),
               ],
             ),
           );
