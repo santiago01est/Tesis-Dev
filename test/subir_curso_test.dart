6ef3e0822cb8563419ae474136224ee54e0d64e2
santiago01est <santiago01est@gmail.com>
2024-05-26 22:44:17 -0500

    anexo test

--- test/subir_curso_test.dart
+++ test/subir_curso_test.dart
@@ -27,23 +27,8 @@ void main() {
 
       expect(snapshot.docs.length, 1);
 
-      print('El test de "Subir curso" se realizó correctamente. Validación de creación correcta.');
-
-      // // 2. Subir unidades relacionadas al curso
-      // await subirUnidadesFB(firestore, docRef.id);
-
-      // // 3. Subir seguimientos relacionados al curso
-      // await guardarSeguimientosFB(firestore, docRef.id);
-
-      // // Asserts para verificar los resultados esperados
-      // final snapshotCursos = await firestore.collection('cursos').get();
-      // expect(snapshotCursos.docs.length, 1);
-
-      // final snapshotUnidades = await firestore.collection('unidades').get();
-      // expect(snapshotUnidades.docs.isNotEmpty, true);
-
-      // final snapshotSeguimientos = await firestore.collection('seguimientos').get();
-      // expect(snapshotSeguimientos.docs.isNotEmpty, true);
+      print(
+          'El test de "Subir curso" se realizó correctamente. Validación de creación correcta.');
     });
     test('Subir unidad', () async {
       final firestore = FakeFirebaseFirestore();
@@ -63,7 +48,8 @@ void main() {
 
       expect(snapshot.docs.length, 1);
 
-      print('El test de "Subir unidad" se realizó correctamente. Validación de creación correcta.');
+      print(
+          'El test de "Subir unidad" se realizó correctamente. Validación de creación correcta.');
     });
     test('Guardar seguimiento', () async {
       final firestore = FakeFirebaseFirestore();
@@ -95,7 +81,8 @@ void main() {
 
       expect(snapshot.docs.length, 1);
 
-      print('El test de "Guardar seguimiento" se realizó correctamente. Validación de creación correcta.');
+      print(
+          'El test de "Guardar seguimiento" se realizó correctamente. Validación de creación correcta.');
     });
   });
 }
